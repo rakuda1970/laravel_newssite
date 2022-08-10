c1c8bbb1d650f6908fd55acc16bc261689aeefd6
rakuda1970 <rakuda1970@gmail.com>
2022-08-10 11:10:07 -0500

    layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,79 +1,115 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+   <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ config('app.name', 'News Site') }}</title>
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <!-- Font Awesome 4.7 -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <div id="app" class="container-fluid mx-0 px-0">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+    <header class="p-3 text-bg-dark">
+         <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+               <a data-bs-target="#collapseExample" data-toggle="collapse" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample" class="btn btn-dark text-white p-0 mx-2 border border-light"><i style="font-size: 1.6em; margin: 0.25em 0.35em 0.25em 0.35em;" class="fa fa-bars" aria-hidden="true"></i></a>
+               <a href="{{ url('/') }}" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+               <img src="https://via.placeholder.com/40/FF0000/FFFFFF/?text=LOGO" alt="{{ config('app.name', 'News Site') }}" class="mx-2">
+               </a>
+               <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                  <li><a href="#" class="nav-link px-2 text-white">US</a></li>
+                  <li><a href="#" class="nav-link px-2 text-white">World</a></li>
+                  <li><a href="#" class="nav-link px-2 text-white">Politics</a></li>
+                  <li><a href="#" class="nav-link px-2 text-white">Business</a></li>
+                  <li><a href="#" class="nav-link px-2 text-white">More</a></li>
+               </ul>
+               <div class="text-end">
+               <a href="#" style="text-decoration: none;" class="px-2 text-white">Audio</a> 
+               <a href="#" style="text-decoration: none;" class="px-2 text-white">Live TV</a>
+               <a data-bs-target="#collapseExample" data-toggle="collapse" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample" class="text-white mx-2"><i class="fa fa-search" aria-hidden="true"></i></a>
+               
+                    @guest
+                         @if (Route::has('login'))
+                        <a class="btn btn-outline-light me-2" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        @endif
 
-                    </ul>
+    
+                    @else
+                        Welcome: {{ Auth::user()->name }} 
+                         <a class="btn btn-outline-light me-2" href="{{ route('logout') }}" 
+                         onclick="event.preventDefault(); 
+                         document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                        </form>
+                    @endguest
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+
+
+
+
+
+
+
+
+               </div>
             </div>
-        </nav>
+         </div>
+      </header>
+      <div class="container-fluid py-3 collapse bg-dark text-white" id="collapseExample">
+         <hr />
+         <form class="col-10 mx-auto mb-3 ">
+          <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+        </form>
+
+         <div class="d-grid gap-3" style="grid-template-columns: 1fr 1fr 1fr 1fr;">
+            <div class="">
+               <ul>
+                  <li class="nav-link px-2 text-white">Lorem ipsum dolor</li>
+                  <li class="nav-link px-2 text-white">sit amet</li>
+                  <li class="nav-link px-2 text-white">consectetur adipiscing elit</li>
+                  <li class="nav-link px-2 text-white">sed do eiusmod</li>
+               </ul>
+            </div>
+            <div class="">
+               <ul>
+                  <li class="nav-link px-2 text-white">Lorem ipsum dolor</li>
+                  <li class="nav-link px-2 text-white">sit amet</li>
+                  <li class="nav-link px-2 text-white">consectetur adipiscing elit</li>
+                  <li class="nav-link px-2 text-white">sed do eiusmod</li>
+               </ul>
+            </div>
+            <div class="">
+               <ul>
+                  <li class="nav-link px-2 text-white">Lorem ipsum dolor</li>
+                  <li class="nav-link px-2 text-white">sit amet</li>
+                  <li class="nav-link px-2 text-white">consectetur adipiscing elit</li>
+                  <li class="nav-link px-2 text-white">sed do eiusmod</li>
+               </ul>
+            </div>
+            <div class="">
+               <ul>
+                  <li class="nav-link px-2 text-white">Lorem ipsum dolor</li>
+                  <li class="nav-link px-2 text-white">sit amet</li>
+                  <li class="nav-link px-2 text-white">consectetur adipiscing elit</li>
+                  <li class="nav-link px-2 text-white">sed do eiusmod</li>
+               </ul>
+            </div>
+            
+         </div>
+         <hr />
+      </div>
 
         <main class="py-4">
             @yield('content')
